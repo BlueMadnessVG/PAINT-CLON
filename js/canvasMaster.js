c8c04a577a8ceb5680e0b29429b0bd84097a2494
BlueMadnessVG <102503098+BlueMadnessVG@users.noreply.github.com>
2023-03-12 22:23:53 -0700

    draw menu and repaint border

--- js/canvasMaster.js
+++ js/canvasMaster.js
@@ -6,9 +6,13 @@ var btnCanvas = document.getElementById("canvas-btn");
 const btnContext = canvaspreview.getContext("2d");
 
 const buttons = document.querySelectorAll(".shapes");
+const configButtons = document.querySelectorAll(".config");
+const menuButtons = document.querySelectorAll(".menu-btn");
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("border");
+var menu = document.getElementById("menu");
+
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
@@ -37,25 +41,118 @@ function makeStruct( names ) {
 var struct = makeStruct("type,sp1,sp2,a,b,border,backgroundColor,borderColor,Layer");
 
 var draw = true;
+var index = 0;
 var X1, X2, Y1, Y2;
+
+function redrawCanvas() {
+
+  for(i = 0; i < Figures.length; i++) {
+    for( y = 0; y < canvas.height; y++ ){
+      for( x = 0; x < canvas.width; x++ ){
+        if ( Figures[i].Layer[x][y] ){
+          redrawPixel(x, y, i);
+        }
+      }
+    }
+  }
+
+}
+
+function menuDisplayNone() {
+
+  menu.style.display = "none";
+  for(i = 0; i < Figures.length; i++) {
+    for( y = 0; y < canvas.height; y++ ){
+      for( x = 0; x < canvas.width; x++ ){
+        if ( Figures[i].Layer[x][y] ){
+          redrawPixel(x, y, i);
+        }
+      }
+    }
+  }
+
+}
+
+function moveFigure(Newindex) {
+
+  const element = Figures.splice(index, 1)[0];
+  Figures.splice(Newindex, 0, element);
+  console.log(Figures);
+
+}
+
+menuButtons.forEach(btn => {
+  btn.addEventListener("click", function(event) {
+    switch (btn.id) {
+      case 'moveUp':
+        if( index != Figures.length - 1 ){
+          moveFigure(index + 1);
+          redrawCanvas();
+        }
+        menuDisplayNone();
+
+        break;
+      case 'moveDown':
+
+        if( index != 0 ){
+          moveFigure(index - 1);
+          redrawCanvas();
+        }
+        menuDisplayNone();
+
+        break;
+      case 'color':
+
+        if( Figures[index].borderColor != borderColor.value ){
+          Figures[index].borderColor = borderColor.value;
+          redrawCanvas();
+        }
+        menuDisplayNone();
+
+        break;
+      case 'clear':
+
+        Figures.splice(index, 1);
+        console.log(Figures);
+
+        rContext.clearRect(0, 0, canvas.width, canvas.height);
+
+        redrawCanvas();
+        menuDisplayNone();
+
+        break;
+    }
+
+  });
+})
 
 buttons.forEach(btn => {
   btn.addEventListener("click", function(event) {
 
     buttons.forEach(btnClass => {
-      btnClass.classList.remove("active");
+      btnClass.classList.remove("activeS");
     });
   
-    btn.classList.add("active");
+    btn.classList.add("activeS");
 
   });
 })
 
+configButtons.forEach(btns => {
+  btns.addEventListener("click", function(event) {
 
+    configButtons.forEach(btnClass => {
+      btnClass.classList.remove("activeC");
+    });
+  
+    btns.classList.add("activeC");
+
+  });
+})
 
 function drawFigure() {
 
-  type = document.getElementsByClassName("active")[0].id;
+  type = document.getElementsByClassName("activeS")[0].id;
 
   switch (type) {
 
@@ -78,31 +175,76 @@ function drawFigure() {
 
 }
 
+function configCanvas() {
+
+  type = document.getElementsByClassName("activeC")[0].id;
+
+  switch(type){
+
+    case 'color':
+
+      for(i = 0; i < Figures.length; i++) {
+        if( Figures[i].Layer[X1][Y1] ){
+          if( Figures[index].borderColor != borderColor.value ){
+            Figures[index].borderColor = borderColor.value;
+          }
+          redrawCanvas();
+        }
+      }
+
+      break;
+
+  }
+
+}
+
+window.addEventListener("contextmenu", e => e.preventDefault());
+
 //TOMA EL PRIMER VALOR PARA HACER UN PREVIEW
 canvas.addEventListener( "mousedown", function(event){
+
+  event.preventDefault();
+  
   rContext.save();
   X1 = event.offsetX;
   Y1 = event.offsetY;
 
-  console.log(buttons);
+  if( event.button == 2  ){
 
-  rContext.fillStyle = borderColor.value;
-  rContextpreview.fillStyle = borderColor.value;
+    for(i = 0; i < Figures.length; i++) {
+      if( Figures[i].Layer[X1][Y1] ){
 
-  for(i = 0; i < Figures.length; i++) {
-    if( Figures[i].Layer[X1][Y1] ){
-      for( y = 0; y < canvas.height; y++ ){
-        for( x = 0; x < canvas.width; x++ ){
-          if ( Figures[i].Layer[x][y] ){
-            selectPixel(x, y, i);
+        menu.style.display = "flex";
+        menu.style.left = event.clientX + "px";
+        menu.style.top = event.clientY + "px"
+
+        for( y = 0; y < canvas.height; y++ ){
+          for( x = 0; x < canvas.width; x++ ){
+            if ( Figures[i].Layer[x][y] ){
+              index = i;
+              selectPixel(x, y, i);
+            }
           }
         }
+        i = Figures.length;
       }
-      i = Figures.length;
+      else {
+        menu.style.display = "none";
+      }
     }
-  }
 
-  draw = false;
+  }
+  else {
+    menuDisplayNone();
+
+    if( document.getElementsByClassName("activeC")[0] != undefined ){
+      configCanvas();
+    }
+
+    rContext.fillStyle = borderColor.value;
+    rContextpreview.fillStyle = borderColor.value;
+    draw = false;
+  }
   
 });
 
@@ -111,7 +253,7 @@ canvas.addEventListener( "mouseup", function( event ) {
   draw = true;
 
   if ( X2 != undefined ) {
-    var prueba = new struct( type, [X1,Y1], [X2,Y2], a, b, slider.value, "0, 0, 0, 0","63,81,181,255", new Array(canvas.width).fill(false).map( () => new Array(canvas.height).fill(false) ) );
+    var prueba = new struct( type, [X1,Y1], [X2,Y2], a, b, slider.value, "0, 0, 0, 0",borderColor.value, new Array(canvas.width).fill(false).map( () => new Array(canvas.height).fill(false) ) );
     Figures.push( prueba );
   }
 
@@ -163,6 +305,11 @@ function drawpix(x,y){
     else {
       rContextpreview.fillRect(x,y,slider.value,slider.value);
     }
+}
+
+function redrawPixel( x, y, L ){
+  rContext.fillStyle = Figures[L].borderColor;
+  rContext.fillRect(x, y, Figures[L].border, Figures[L].border);
 }
   
 function selectPixel(x, y, L){
